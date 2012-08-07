308da06593d0a4ddfeaefb09f4886faab6f92309
Cheol Kang <cornchz@gmail.com>
2012-08-08 01:49:25 +0900

    unicode, str 타입 모두 받을 수 있도록 개선

--- nero/romanize.py
+++ nero/romanize.py
@@ -1,23 +1,22 @@
 #! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-from hangul import translit
-import utils
+from utils import firstname2eng, force_unicode, lastname2eng, romanize
 
+@force_unicode
 def name2eng(name):
-    syls = utils.getsyls(name)
-    last_syl = syls[0]
-    first_syls = syls[1:]
-    name_en =  utils.firstname2eng(first_syls)\
-            + ' ' + utils.lastname2eng(last_syl)
+    last_syl, first_syls = name[0], name[1:]
+    lastname, firstname = lastname2eng(last_syl), firstname2eng(first_syls)
+    name_en =  u'%s %s' % (lastname, firstname)
     return name_en
 
+
+@force_unicode
 def party2eng(party):
-    if party == '무소속':
-        party = 'non-party'
-    else: 
-        if party.endswith('당'):
-            party = translit.romanize(party[:-3]) + ' Party'
-        else:
-            party = translit.romanize(party)
-    return capname(party)
+    if party == u'무소속':
+        return u'independent'
+
+    if party.endswith(u'당'):
+        party = party[:-1]
+
+    return romanize(party).capitalize()
--- nero/utils.py
+++ nero/utils.py
@@ -7,45 +7,66 @@ import crawlers
 # List family names of (estimated population) > 50M
 # Source: http://en.wikipedia.org/w/index.php?title=List_of_Korean_family_names
 LAST_NAMES = {
-    "김": "Kim",
-    "이": "Lee",
-    "박": "Park",
-    "정": "Jeong",
-    "최": "Choi",
-    "조": "Cho",
-    "강": "Kang",
-    "윤": "Yoon",
-    "장": "Jang",
-    "신": "Shin",
-    "한": "Han",
-    "오": "Oh",
-    "서": "Seo",
-    "전": "Jeon",
-    "권": "Kwon",
-    "황": "Hwang",
-    "송": "Song",
-    "안": "Ahn",
-    "임": "Lim",
-    "류": "Ryu",
-    "고": "Ko"
+    u"김": u"Kim",
+    u"이": u"Lee",
+    u"박": u"Park",
+    u"정": u"Jeong",
+    u"최": u"Choi",
+    u"조": u"Cho",
+    u"강": u"Kang",
+    u"윤": u"Yoon",
+    u"장": u"Jang",
+    u"신": u"Shin",
+    u"한": u"Han",
+    u"오": u"Oh",
+    u"서": u"Seo",
+    u"전": u"Jeon",
+    u"권": u"Kwon",
+    u"황": u"Hwang",
+    u"송": u"Song",
+    u"안": u"Ahn",
+    u"임": u"Lim",
+    u"류": u"Ryu",
+    u"고": u"Ko"
     }
-    
-def getsyls(name):
-    syls = [syl.encode('utf-8') for syl in list(name.decode('utf-8'))]
-    return syls
 
+def force_unicode(f):
+    '''Python 2.x 기준, 유니코드가 아닌 str타입이 인자로 들어오면
+    유니코드로 변환한 뒤 원 함수에 인자로 넘기고, 그 결과는 다시
+    원래의 인코딩으로 변환해서 반환하는 decorator'''
+
+    def interface(txt, encoding='utf-8'):
+
+        # decode to unicode
+        if isinstance(txt, str):
+            txt = txt.decode(encoding)
+
+        # actual processing
+        result = f(txt)
+
+        # return re-encoded results
+        return result.encode(encoding)
+
+    return interface
+
+@force_unicode
+def romanize(txt):
+    '''한글을 영문으로 변환'''
+
+    return translit.romanize(txt.encode('utf-8')).decode('utf-8')
+
+@force_unicode
 def firstname2eng(syls):
-    syls = [translit.romanize(syl) for syl in syls]
+    '''한글 이름을 영문으로 변환'''
+
+    syls = (romanize(syl) for syl in syls)
     firstname_en = '-'.join(syls)
-    return capname(firstname_en)
+    return firstname_en.capitalize()
 
+@force_unicode
 def lastname2eng(syl):
-    # TODO: 영문 성씨 순위 데이터 구해서 높은 percentage의 성씨로 변환
-    try:
-        lastname_en = LAST_NAMES[syl]
-    except:
-        lastname_en = translit.romanize(syl)
-    return capname(lastname_en)
+    '''한글 성을 영문으로 변환'''
 
-def capname(string):
-    return string[0].capitalize() + string[1:]
+    # TODO: 영문 성씨 순위 데이터 구해서 높은 percentage의 성씨로 변환
+    lastname_en = LAST_NAMES.get(syl, romanize(syl))
+    return lastname_en.capitalize()

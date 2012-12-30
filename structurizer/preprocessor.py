e3add85d65d46bbecf633b3b95a561e417433e85
Lucy Park <525600min@gmail.com>
2012-12-30 21:41:50 +0900

    Add preprocessing methods

--- structurizer/preprocessor.py
+++ structurizer/preprocessor.py
@@ -1,42 +1,66 @@
-#! /usr/bin/python3
+#! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+import re
 import utils
 
 def preprocessor(fieldlist):
     dic = utils.read_HHdic()
 
+    #fieldlist = unicodify(fieldlist)
     fieldlist = nullify(fieldlist)
-    #fieldlist = remove_periods(fieldlist)
+    fieldlist = remove_commas(fieldlist)
+    fieldlist = remove_parentheses(fieldlist)
     fieldlist = trans_hanja(fieldlist, dic)
     #fieldlist = trans_roman(fieldlist)
-    fieldlist = check_parenthesis(fieldlist)
-
+    #fieldlist = check_parenthesis(fieldlist)
     return list(fieldlist)
+
+# string to unicode
+def _unicodify(item, encoding='utf-8'):
+    if isinstance(item, basestring):
+        if not isinstance(item, unicode):
+            item = unicode(item, encoding)
+    return item
+
+def unicodify(fieldlist):
+    return (_unicodify(item) for item in fieldlist)
 
 # nullify 'null', '미기재' cells
 def _nullify(item):
-    if item=='미기재':
+    if item==u'미기재':
         item = ''
     return item
 
 def nullify(fieldlist):
     return (_nullify(item) for item in fieldlist)
 
-# eliminate periods
-def remove_periods(fieldlist):
-    return (item.replace('.','') for item in fieldlist)
+# eliminate commas
+def remove_commas(fieldlist):
+    return (item.replace(',','') for item in fieldlist)
+
+# eliminate parentheses
+def remove_parentheses(fieldlist):
+    return (re.sub(r'[\(\)]', ' ', item) for item in fieldlist)
 
 # transliterate hanja to hangul
 def trans_hanja(fieldlist, dic):
     return (utils.hanja2hangul(dic, item) for item in fieldlist)
 
-# check parentheses
+# check word in parentheses and delete if repetitive
 def _check_parenthesis(item):
     s, e = item.find('('), item.find(')')
-    inword, outword = item[s+1:e], item[2*s-e+1:s] 
-    if inword==outword:
-        item = item.replace(item[s:e+1],'')
+
+    if s==-1 and e==-1:
+        pass
+    else:
+        inword = item[s+1:e]
+        if item[s-1]==u' ':
+            outword = item[2*s-e:s-1] 
+        else:
+            outword = item[2*s-e+1:s] 
+        if inword==outword:
+            item = item.replace(item[s:e+1],'')
     return item
 
 def check_parenthesis(fieldlist):

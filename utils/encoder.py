a4eb6c90965c368afec5818c5c75cf4eb72ba7ea
Lucy Park <me@lucypark.kr>
2013-04-18 02:58:13 +0900

    Add region encoder

---
+++ utils/encoder.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import csv
+import pandas as pd
+
+def get_codemap(opt, lang='ko'):
+    cb = pd.read_csv('/home/e9t/dev/popong/nlp/_input/cb-%s.csv' % opt,
+            encoding='utf-8')
+    return dict(zip(cb[lang], cb['code']))
+
+def encode(word, codemap):
+    try:
+        return int(codemap[word])
+    except:
+        return None
+
+if __name__=='__main__':
+    cm = get_codemap('region')
+    print encode(u'서울특별시', cm)

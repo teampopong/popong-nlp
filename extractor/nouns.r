32036b629906ae758c7af10a4a6603dff97beefa
Cheol Kang <steel@popong.com>
2014-03-24 11:47:25 +0900

    Stop transmitting through file and replace it to pipes.

--- extractor/nouns.r
+++ extractor/nouns.r
@@ -2,13 +2,10 @@
 
 library('KoNLP')
 
-setwd('/home/e9t/dev/popong/nlp/extractor')
-tmp <- file.create('nouns.txt')
-
 f <- file("stdin")
 open(f)
 while(length(line<- readLines(f, n=1)) > 0) {
   noun <- extractNoun(line)
   nouns <- paste(noun, collapse=" ")
-  write(nouns, 'nouns.txt', append=TRUE)
+  write(nouns, stdout(), append=TRUE)
 }

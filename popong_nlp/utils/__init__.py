faafa318e9d6154cf149ce6f3a80003028a15f65
IU <dev@popong.com>
2019-08-31 08:01:02 +0000

    futurize codes

--- popong_nlp/utils/__init__.py
+++ popong_nlp/utils/__init__.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python2.7
 
-import counter
+from __future__ import absolute_import
+#! /usr/bin/python2.7
+
+from . import counter
 
 def count(string):
     return counter.count(string)
